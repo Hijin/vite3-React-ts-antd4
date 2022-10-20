2a3414b82c8bcfc8ff4dc59409ad1ad2d7566497
陈南进 <jlee.chen@leaflongtx.com>
2022-10-20 10:20:28 +0800

    别名设置

--- src/service/request.ts
+++ src/service/request.ts
@@ -7,7 +7,7 @@ import {message} from 'antd';
 // 错误信息，防止多次提示
 let errMsg = "";
 
-const service = axios.create({ // baseURL:`${process.env.VUE_APP_BASE_URL}/api`, // 自动配置的后台的请求的地址
+const service:any = axios.create({ // baseURL:`${process.env.VUE_APP_BASE_URL}/api`, // 自动配置的后台的请求的地址
   timeout: 300 * 1000
   // 设置请求的超时时间
   // headers: {'Content-Type': 'application/json'},
@@ -18,20 +18,20 @@ const service = axios.create({ // baseURL:`${process.env.VUE_APP_BASE_URL}/api`,
 /**
   * 需求:自定义请求拦截器
   */
-service.interceptors.request.use((config) => {
+service.interceptors.request.use((config:any) => {
   let token = '';
   if (token) {
     config.headers.Authorization = token;
   }
   return config;
-}, (error) => {
+}, (error:any) => {
   return Promise.reject(error);
 });
 
 /**
   * 需求:自定义响应拦截去
   */
-service.interceptors.response.use((response) => {
+service.interceptors.response.use((response:any) => {
   const res = response.data;
   if (res.code === 200) {
     return Promise.resolve(res);
@@ -39,7 +39,7 @@ service.interceptors.response.use((response) => {
     tostMsg(res.desc);
     return Promise.reject(res);
   }
-}, (error) => {
+}, (error:any) => {
   const res = error.response.data;
   if (res.code === 401) {
     tostMsg("token已过期,请重新登录！");
@@ -53,43 +53,43 @@ service.interceptors.response.use((response) => {
 
 export default service;
 
-export function putMethod(url, data, showSuccessMsg) {
+export function putMethod(url:string, data:any, showSuccessMsg:boolean) {
   return serviceSend(url, data, "put", showSuccessMsg);
 }
 
-export function deleteMethod(url, data, showSuccessMsg) {
+export function deleteMethod(url:string, data:any, showSuccessMsg:boolean) {
   return serviceSend(url, {
     params: data
   }, "delete", showSuccessMsg);
 }
 
-export function postMethod(url, data = {}, showSuccessMsg) {
+export function postMethod(url:string, data:any={}, showSuccessMsg:boolean) {
   return serviceSend(url, data, "post", showSuccessMsg);
 }
 
-export function getMethod(url, params, showSuccessMsg) {
+export function getMethod(url:string, params:any={}, showSuccessMsg:boolean) {
   return serviceSend(url, {
     params: params
   }, "get", showSuccessMsg);
 }
 
-function serviceSend(url, params = {}, method = "post", showSuccessMsg = false) {
+function serviceSend(url:string, params = {}, method = "post", showSuccessMsg = false) {
   filterParams(params);
   return new Promise((resolve, reject) => {
-    service[method](url, params).then((res) => {
+    service[method](url, params).then((res:any) => {
       resolve(res.data);
       showSuccessMsg && res.desc && message.success(res.desc);
-    }).catch((error) => {
+    }).catch((error:any) => {
       reject(error);
     });
   });
 }
 
-function tostMsg(msg) {
+function tostMsg(msg:string) {
   if (errMsg.length) return
   errMsg = msg;
   message.error(errMsg);
-  let t = setTimeout(() => {
+  let t:any = setTimeout(() => {
     errMsg = "";
     clearTimeout(t)
     t = null
@@ -97,7 +97,7 @@ function tostMsg(msg) {
 }
 
 // 过滤无效参数
-function filterParams(params) {
+function filterParams(params:any) {
   for (let key in params) {
     const val = params[key];
     if (Object.prototype.toString.call(val) === "[object Object]" || Object.prototype.toString.call(val) === "[object Array]") {
