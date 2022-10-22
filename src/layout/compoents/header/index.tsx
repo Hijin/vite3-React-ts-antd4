bb9007b0122dbd6b7bf4647e151488efd630e0d3
陈南进 <jlee.chen@leaflongtx.com>
2022-10-22 10:43:41 +0800

    feat: header

--- src/layout/compoents/header/index.tsx
+++ src/layout/compoents/header/index.tsx
@@ -1,6 +1,15 @@
 import './index.less';
+import { Image } from 'antd';
+import { LogoImg } from '@/assets/imgs';
 const Header = () => {
-  return <div className="home-layout-header"></div>;
+  return (
+    <div className="home-layout-header flex-b">
+      <div className="flex-h-c">
+        <Image src={LogoImg} className="home-layout-header__logo" />
+        <div className="home-layout-header__name">项目名称</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
