11d446a76450847d72d053d303d6462356aa13e6
陈南进 <jlee.chen@leaflongtx.com>
2022-10-22 14:55:11 +0800

    feat: 导航条/侧边栏

--- src/assets/imgs/index.tsx
+++ src/assets/imgs/index.tsx
@@ -1,1 +1,5 @@
 export { default as LogoImg } from './logo.png';
+export { default as AvatarDefaultImg } from './avatar_default.png';
+export { default as HeaderHomeSelImg } from './header_home_sel.png';
+export { default as HeaderPatientSelImg } from './header_patient_sel.png';
+export { default as HeaderVisitSelImg } from './header_visit_sel.png';
--- src/layout/compoents/header/index.tsx
+++ src/layout/compoents/header/index.tsx
@@ -1,13 +1,113 @@
+import { useState } from 'react';
+import { Image, Dropdown, Menu } from 'antd';
+import { useNavigate, useLocation } from 'react-router-dom';
+import cs from 'classnames';
+import {
+  LogoImg,
+  AvatarDefaultImg,
+  HeaderHomeSelImg,
+  HeaderPatientSelImg,
+  HeaderVisitSelImg
+} from '@/assets/imgs';
 import './index.less';
-import { Image } from 'antd';
-import { LogoImg } from '@/assets/imgs';
+
+const DropdownMenuItems = [
+  { key: 'password', label: '设置密码', path: '/researchpc/login' },
+  { key: 'loginOut', label: '退出登录', path: '/researchpc/setting' }
+];
+const HeaderNavItems = [
+  {
+    key: 'home',
+    label: '首页',
+    icon: LogoImg,
+    selIcon: HeaderHomeSelImg,
+    path: ''
+  },
+  {
+    key: 'patient',
+    label: '患者管理',
+    icon: LogoImg,
+    selIcon: HeaderPatientSelImg,
+    path: ''
+  },
+  {
+    key: 'visit',
+    label: '随访管理',
+    icon: LogoImg,
+    selIcon: HeaderVisitSelImg,
+    path: ''
+  }
+];
+
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [navCurKey, setNavCurKey] = useState();
+
+  const handleNavClick = (item: any) => {
+    setNavCurKey(item.key);
+    item.path && navigateToPath(item.path);
+  };
+  const handleMenuClick = (e: any) => {
+    const { key } = e;
+    const clickMenu: any = DropdownMenuItems.find((v) => v.key === key);
+    navigateToPath(clickMenu.path);
+  };
+  const navigateToPath = (path: string) => {
+    const { pathname } = location;
+    if (pathname === path) return;
+    navigate(path);
+  };
+  const DropdownMenu = (
+    <Menu onClick={handleMenuClick} items={DropdownMenuItems} />
+  );
   return (
     <div className="home-layout-header flex-b">
-      <div className="flex-h-c">
-        <Image src={LogoImg} className="home-layout-header__logo" />
-        <div className="home-layout-header__name">项目名称</div>
+      <div className="flex-h-c flex-1">
+        <div className="flex-h-c">
+          <Image src={LogoImg} className="home-layout-header__logo" />
+          <div className="col-w ft-18 ft-b">项目名称</div>
+        </div>
+        <div className="flex-1 flex-h-c h-full ml-20">
+          {HeaderNavItems.map((v) => (
+            <div
+              key={v.key}
+              className={cs('home-layout-header__nav-item pointer flex-h-c', {
+                'bg-w': navCurKey === v.key
+              })}
+              onClick={() => handleNavClick(v)}
+            >
+              <Image
+                src={navCurKey === v.key ? v.selIcon : v.icon}
+                className="home-layout-header__nav-item__icon"
+              />
+              <div
+                className={cs('home-layout-header__nav-item__label ml-15', {
+                  'home-layout-header__nav-item__label--activity':
+                    navCurKey === v.key
+                })}
+              >
+                {v.label}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <Dropdown
+        className="flex-h-c"
+        overlay={DropdownMenu}
+        placement="bottomRight"
+      >
+        <div>
+          <Image
+            src={AvatarDefaultImg}
+            className="home-layout-header__avatar"
+          />
+          <div className="home-layout-header__user-name col-w ft-14 ml-10">
+            王医生
+          </div>
+        </div>
+      </Dropdown>
     </div>
   );
 };
