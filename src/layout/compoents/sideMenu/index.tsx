4a3bb1807ddabc6655cdc6c8df7c6da4d3547cbd
陈南进 <jlee.chen@leaflongtx.com>
2022-10-25 19:58:24 +0800

    feat: menu

--- src/layout/compoents/sideMenu/index.tsx
+++ src/layout/compoents/sideMenu/index.tsx
@@ -1,19 +1,27 @@
 import { useState, useEffect } from 'react';
 import { Menu } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import cs from 'classnames';
 import { homeMenus } from '@/routes';
 import './index.less';
 
 const SideMenu = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [menuItems, setMenuItems] = useState<any[]>([]);
-  const [curKey, setCurKey] = useState();
+  const [curKey, setCurKey] = useState<any>();
+  const [selectedKeys, setSelectedKeys] = useState<any[]>([]);
   const [collapsed, setCollapsed] = useState(false);
   useEffect(() => {
     const menus = initMenus(homeMenus);
     setMenuItems(menus);
   }, []);
+  useEffect(() => {
+    const { pathname } = location
+    const keysArr = pathname.split('/').slice(2)
+    setSelectedKeys(keysArr)
+    setCurKey(keysArr.slice(-1))    
+  }, [location])
   const initMenus = (data: any[]) => {
     const menus = data
       .filter((m: any) => !m.hideInMenu)
@@ -35,7 +43,9 @@ const SideMenu = () => {
     return menus;
   };
   const handleMenuClick = (e: any) => {
-    const { keyPath, key } = e;
+    console.log(222);
+    
+    const { keyPath, key } = e;    
     if (key === curKey) return;
     setCurKey(key);
     const path = keyPath.reverse().join('/');
@@ -51,7 +61,7 @@ const SideMenu = () => {
       <Menu
         className="h-full"
         onClick={handleMenuClick}
-        mode="inline"
+        selectedKeys={selectedKeys}
         items={menuItems}
       />
       <div
